2ab031f4febbe71619710e56085f0572a2412239
KarolBurbanoN <karol.burbano@correounivalle.edu.co>
2025-10-01 17:02:17 -0500

    Se añade archivo principal

---
+++ problema_riego.py
@@ -0,0 +1,18 @@
+def roFB(finca):
+    """
+    Fuerza bruta: recibe `finca` (lista de tuplas (ts,tr,p))
+    Devuelve (pi, costo)
+    """
+    pass
+def roV(finca):
+    """
+    Algoritmo voraz propuesto.
+    Devuelve (pi, costo)
+    """
+    pass
+def roPD(finca):
+    """
+    Programaci´on din´amica.
+    Devuelve (pi, costo)
+    """
+    pass
